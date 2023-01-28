bce5759affa6862fe1e04054c4f45bb2ee25137b
Jamie Coleman <j.a.coleman1978@gmail.com>
2023-01-27 22:24:09 -0600

    Display gitBalancingHint message when appropriate

--- src/components/balancing-equations/BalancingQuestion.tsx
+++ src/components/balancing-equations/BalancingQuestion.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useMemo } from "react";
 import { Button } from "react-bootstrap";
+import { useToggle } from "../../customHooks/useToggle";
 import Arrow from "../common/Arrow";
 import CoefficientInput from "./CoefficientInput";
 import { useCoefficientInputs } from "../../customHooks/useCoeffcientInputs";
@@ -10,67 +11,76 @@ import { ReactionTypeList } from "../common/configurations/types";
 const BalancingQuestion = ({toggleFlag, equation}: {toggleFlag: boolean, equation: ReactionTypeList}) => {
     const equationParts = useMemo(() => makeEquationParts(equation), [equation]);
 
-    const [coefficientInputs, handleCoefficientInputs, inputColor, handleUpdateInputColor] = useCoefficientInputs();
+    const [answerCheckFlag, setAnswerCheckFlag] = useToggle();
 
+    const [coefficientInputs, handleCoefficientInputs, inputColor, handleUpdateInputColor] = useCoefficientInputs();
     
     useEffect(() => {
-      console.log(getBalancingHint(coefficientInputs, equationParts));
       handleCoefficientInputs("all", "");
+      setAnswerCheckFlag();
     }, [toggleFlag, handleCoefficientInputs])
 
   return (
     <section className="balancing-section med-gap border-bubble">
-      <div className="balancing">
-        <div className="flex-left-center sm-gap reactants">
-          <CoefficientInput 
-            equationPart={equationParts.R1} 
-            formStyle={inputColor} 
-            userAnswer={coefficientInputs.R1} 
-            handleCoefficientChange={handleCoefficientInputs} 
-          />
-
-          {equation.type !== "decomposition" ? <div>+</div> : null}
-
-          {equation.type !== "decomposition" ? 
+      <div className="flex-column">
+        <div className="balancing">
+          <div className="flex-left-center sm-gap reactants">
             <CoefficientInput 
-              equationPart={equationParts.R2} 
+              equationPart={equationParts.R1} 
               formStyle={inputColor} 
-              userAnswer={coefficientInputs.R2} 
+              userAnswer={coefficientInputs.R1} 
               handleCoefficientChange={handleCoefficientInputs} 
-            /> 
-            : null
-          }
+            />
+
+            {equation.type !== "decomposition" ? <div>+</div> : null}
+
+            {equation.type !== "decomposition" ? 
+              <CoefficientInput 
+                equationPart={equationParts.R2} 
+                formStyle={inputColor} 
+                userAnswer={coefficientInputs.R2} 
+                handleCoefficientChange={handleCoefficientInputs} 
+              /> 
+              : null
+            }
+          </div>
+
+          <Arrow />
+
+          <div className="flex-left-center sm-gap products">
+            <CoefficientInput 
+              equationPart={equationParts.P1} 
+              formStyle={inputColor} 
+              userAnswer={coefficientInputs.P1} 
+              handleCoefficientChange={handleCoefficientInputs} 
+            />
+
+            {equation.type !== "combination" ? <div>+</div> : null}
+
+            {equation.type !== "combination" ? 
+              <CoefficientInput 
+                equationPart={equationParts.P2} 
+                formStyle={inputColor} 
+                userAnswer={coefficientInputs.P2} 
+                handleCoefficientChange={handleCoefficientInputs} 
+              /> 
+              : null
+            }
+          </div>
         </div>
 
-        <Arrow />
-
-        <div className="flex-left-center sm-gap products">
-          <CoefficientInput 
-            equationPart={equationParts.P1} 
-            formStyle={inputColor} 
-            userAnswer={coefficientInputs.P1} 
-            handleCoefficientChange={handleCoefficientInputs} 
-          />
-
-          {equation.type !== "combination" ? <div>+</div> : null}
-
-          {equation.type !== "combination" ? 
-            <CoefficientInput 
-              equationPart={equationParts.P2} 
-              formStyle={inputColor} 
-              userAnswer={coefficientInputs.P2} 
-              handleCoefficientChange={handleCoefficientInputs} 
-            /> 
-            : null
-          }
-        </div>
+        {answerCheckFlag && inputColor.backgroundColor === "lightpink" 
+          ? <p className="hint-text">{getBalancingHint(coefficientInputs, equationParts)}</p> 
+          : null
+        }
       </div>
 
       <div className="flex-left-center">
           <Button 
             variant="success" 
             className="flex-center-center check-answer-btn" 
-            onClick={() => handleUpdateInputColor(coefficientInputs, equationParts)}
+            onClick={() => {handleUpdateInputColor(coefficientInputs, equationParts)
+                            setAnswerCheckFlag()}}
           >
               Check Answer
           </Button>
